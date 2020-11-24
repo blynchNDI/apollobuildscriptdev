c8306ce559afed1a3f45a5ec2df0b18ef3691e88
Ben Lynch <31080607+blynchNDI@users.noreply.github.com>
2020-11-23 19:56:24 -0500

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -2,6 +2,5 @@
 
 cd /home/ubuntu/apollo
 sudo docker-compose down
-rm /home/ubuntu/settings.ini || true
-mv /home/ubuntu/apollo/settings.ini /home/ubuntu/ || true
+cp /home/ubuntu/apollo/settings.ini /home/ubuntu/ || true
 rm /home/ubuntu/apollo/settings.ini || true
